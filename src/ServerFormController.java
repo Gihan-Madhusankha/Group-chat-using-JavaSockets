04dd1d35f5c541f7ca0ae51197b7550d42cd608e
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-08-10 23:41:20 +0530

    Content update

--- src/ServerFormController.java
+++ src/ServerFormController.java
@@ -18,6 +18,11 @@ public class ServerFormController {
 
             while (!serverSocket.isClosed()) {
                 Socket localSocket = serverSocket.accept();
+                System.out.println("Client accepted...");
+
+
+
+
             }
 
         } catch (IOException e) {
