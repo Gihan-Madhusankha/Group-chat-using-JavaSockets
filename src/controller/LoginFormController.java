f4f3b9581bf2f9ca57071cd39795adfefa134101
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-08-13 21:35:09 +0530

    Create login components

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,9 +1,20 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * @author : Gihan Madhusankha
  * 2022-08-13 3:17 PM
  **/
 
 public class LoginFormController {
+    public JFXTextField txtUsername;
+    public AnchorPane loginContext;
+
+    public void loginBtnOnAction(ActionEvent actionEvent) {
+
+
+    }
 }
